280ee46c52aa3271c55353721f9901bfff8603cf
suraj-singh-rathore <ssingh70115@gmail.com>
2025-10-17 08:31:23 +0530

    revise fragments and Map methods

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -1,16 +1,37 @@
+//18. Fragments:-
 
+// function App() {
+//   return (
+//   <>
+//   <h1>Healthy Food</h1>
+//     <ul className="list-group">
+//       <li className="list-group-item">Dal</li>
+//       <li className="list-group-item">Green Vegetable</li>
+//       <li className="list-group-item">Bread</li>
+//       <li className="list-group-item">Milk</li>
+//        <li className="list-group-item">Sprouts</li>
+//      </ul>
+//     </>
+//   );
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//19. Map Method:-
 
 function App() {
+  let foodItems = ["Dal", "Green Vegetables", "Bread", "Milk", "Sprouts", "Ghee"];
+
   return (
-  <>
-  <h1>Healthy Food</h1>
-    <ul class="list-group">
-      <li class="list-group-item">Dal</li>
-      <li class="list-group-item">Green Vegetable</li>
-      <li class="list-group-item">Bread</li>
-      <li class="list-group-item">Milk</li>
-       <li class="list-group-item">Sprouts</li>
-     </ul>
+    <>
+    <h1>Healthy Food</h1>
+    <ul className="list-group">
+      {foodItems.map((item) => (
+        <li key={item} className="list-group-item">{item}</li>
+      ))}
+    </ul>
     </>
   );
 }
