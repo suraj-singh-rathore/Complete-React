50a81584ed9f9f95478f4d70dac3f9c5e202fad1
suraj-singh-rathore <ssingh70115@gmail.com>
2025-10-20 22:07:29 +0530

    update changes

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -1,4 +1,4 @@
-//18. Fragments:-
+//18. FRAGMENTS:-
 
 // function App() {
 //   return (
@@ -19,21 +19,72 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-//19. Map Method:-
+//19. MAP METHOD:-
+
+// function App() {
+//   let foodItems = ["Dal", "Green Vegetables", "Bread", "Milk", "Sprouts", "Ghee"];
+
+//   return (
+//     <>
+//     <h1>Healthy Food</h1>
+//     <ul className="list-group">
+//       {foodItems.map((item) => (
+//         <li key={item} className="list-group-item">{item}</li>
+//       ))}
+//     </ul>
+//     </>
+//   );
+// }
+
+// export default App;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// CONDITIONAL STATEMENT:-
+// if-else statement:-
+
+// function App() {
+//   let foodItems = [];
+//   //let foodItems = ["Dal", "Ghee", "Bread", "Milk", "Sprouts"];
+
+//   if (foodItems.length === 0) {
+//     return <h3>No food items are available.</h3>;
+//   }
+
+//   return (
+//     <>
+//     <h1>Healthy Food</h1>
+//     <ul className="list-group">
+//       {foodItems.map((item) => (
+//         <li key={item} className="list-group-item">{item}</li>
+//       ))}
+//       </ul> 
+//     </>
+//   )
+// }
+
+// export default App;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TERNARY OPERATOR:-
 
 function App() {
-  let foodItems = ["Dal", "Green Vegetables", "Bread", "Milk", "Sprouts", "Ghee"];
+  //let foodItems = [];
+  let foodItems = ["Dal", "Ghee", "Bread", "Milk", "Sprouts"];
+
+  let emptyMessage = foodItems.length === 0 ? <h3>No items are available</h3> : null;
 
   return (
     <>
     <h1>Healthy Food</h1>
+    {emptyMessage}
+    {/* {foodItems.length === 0 ? <h3>No items are available.</h3> : null} */}
     <ul className="list-group">
       {foodItems.map((item) => (
         <li key={item} className="list-group-item">{item}</li>
       ))}
-    </ul>
+      </ul> 
     </>
-  );
+  )
 }
 
 export default App;
